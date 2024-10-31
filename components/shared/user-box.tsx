1600f6aff02dcb08964e2cd954bec0c81cbd2fe1
Jakhong1rbek <yokubjonovjakhongirbek@gmail.com>
2024-11-01 03:56:28 +0900

    Bug Fix

--- components/shared/user-box.tsx
+++ components/shared/user-box.tsx
@@ -1,22 +1,21 @@
 'use client'
+
 import { SignOutButton, useUser } from '@clerk/nextjs'
-import React from 'react'
 import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
 	DropdownMenuSeparator,
-	DropdownMenuTrigger,
 } from '../ui/dropdown-menu'
+import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
 import { Avatar, AvatarImage } from '../ui/avatar'
 import Link from 'next/link'
-import { LogOutIcon, UserRoundCog } from 'lucide-react'
-
 import useTranslate from '@/hooks/use-translate'
 
 function UserBox() {
 	const { user } = useUser()
 	const t = useTranslate()
+
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
@@ -30,16 +29,18 @@ function UserBox() {
 				alignOffset={11}
 				forceMount
 			>
-				<div className='flex flex-col space-y-4 p-4'>
+				<div className='flex flex-col space-y-4 p-2'>
 					<p className='text-xs font-medium leading-none text-muted-foreground'>
 						{user?.emailAddresses[0].emailAddress}
 					</p>
+
 					<div className='flex items-center gap-x-2'>
 						<div className='rounded-md bg-secondary p-1'>
 							<Avatar className='size-8'>
-								<AvatarImage src={user?.imageUrl}></AvatarImage>
+								<AvatarImage src={user?.imageUrl} />
 							</Avatar>
 						</div>
+
 						<div className='space-y-1'>
 							<p className='line-clamp-1 font-space-grotesk text-sm'>
 								{user?.fullName}
@@ -47,15 +48,17 @@ function UserBox() {
 						</div>
 					</div>
 				</div>
+
 				<DropdownMenuSeparator />
 				<Link href={'/user-profile'}>
 					<DropdownMenuItem className='w-full cursor-pointer text-muted-foreground'>
-						<UserRoundCog />
-						<span>{t('manageAccount')}</span>
+						{t('manageAccount')}
 					</DropdownMenuItem>
 				</Link>
-				<DropdownMenuItem className='w-full cursor-pointer text-muted-foreground '>
-					<LogOutIcon />
+				<DropdownMenuItem
+					asChild
+					className='w-full cursor-pointer text-muted-foreground'
+				>
 					<SignOutButton>{t('logout')}</SignOutButton>
 				</DropdownMenuItem>
 			</DropdownMenuContent>
